459a7250e44d8b1755e26546aa8fa65039137f70
Иван Сабиров <sabirivan@yandex.com>
2023-12-02 21:40:35 +0600

    Update files

---
+++ commands.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+TELEGRAM_BOT_TOKEN = "6439134880:AAFmRHrKP9yHYxMi2OEqrolJTdUnmGfJOBA"
+
+bot = Bot(token = TELEGRAM_BOT_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands = ["start"])
+async def cmd_start(message: types.Message):
+    await message.answer("Привет! Я бот-пересмешник! Напишите мне что-нибудь!")
+
+@dp.message_handler(content_types = ["text"])
+async def handle_text(message: types.Message):
+    text = message.text
+    
+    await message.reply(text)
+
+def main():
+    executor.start_polling(dp, skip_updates=True)
